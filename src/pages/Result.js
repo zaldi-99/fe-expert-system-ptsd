4bc8408ed8b18d388f2dd5f80d682025c7214fbe
zaldi-99 <rizkyzaldi@gmail.com>
2022-05-16 21:53:02 +0700

    refactoring and compliting function

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,23 +1,31 @@
-import React, { useContext, useEffect, useState } from "react";
-import { AnswerContext } from "../components/Context";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Result = () => {
-  const ctx = useContext(AnswerContext);
-
   const [result, setResult] = useState([]);
-
-  //   useEffect(() => {
-  //     localStorage.setItem("temp", JSON.stringify(ctx.answers));
-  //   }, [ctx]);
+  const [trauma, setTrauma] = useState();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const d = JSON.parse(localStorage.getItem("temp"));
+    setTrauma(d[0]);
     setResult(d);
   }, []);
 
   const forwardChaining = () => {
-    if (result[0].answer === 1) {
-      return <p>Hasil positif</p>;
+    const positif = "Hasil Positif";
+    const negatif = "Hasil Negatif";
+    // If user not experiencing trauma
+    if (trauma.answer === 0) {
+      return negatif;
+    }
+    // Experiencing one or more intrusion symptom
+    if (result[1].answer === 1 || result[2].answer === 1) {
+      return positif;
+    }
+    // Experiencing one or more avoidance symptom
+    if (result[3].answer === 1 || result[4].answer === 1) {
+      return positif;
     } else {
       return <p>Hasil ...</p>;
     }
@@ -26,14 +34,36 @@ const Result = () => {
   return (
     <div>
       <p>Result</p>
+      <div className="answer-conclusion">
+        <p>Jawaban yang anda berikan sebagai berikut :</p>
+        {result
+          .filter(ans => ans.answer === 1)
+          .map(ans => (
+            <div>
+              <ul>
+                <li> {ans.question} </li>
+              </ul>
+            </div>
+          ))}
 
-      {result.map(ans => (
+        {result.length !== 0 && forwardChaining()}
+      </div>
+
+      {/* {result.map(ans => (
         <p key={ans.question}>
           {ans.answer} {ans.question}
         </p>
-      ))}
+      ))} */}
 
-      {result.length !== 0 && forwardChaining()}
+      <button
+        onClick={() => {
+          navigate("/");
+          localStorage.clear();
+          window.location.reload();
+        }}
+      >
+        Selesai
+      </button>
     </div>
   );
 };
